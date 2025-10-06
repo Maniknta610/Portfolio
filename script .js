14e0523c02d9b1c8b8f754de3705842a7f704ac6
manikanta Uppunutala <manikantauppunutala@gmail.com>
2025-10-06 11:48:16 +0530

    created seperate page

--- script .js
+++ script .js
@@ -1,37 +1,37 @@
-const container = document.querySelector('.container');
 
+const container = document.querySelector('.container');
 const cloneContainer = container.cloneNode(true);
 cloneContainer.id = 'dark-container';
 document.body.appendChild(cloneContainer);
 cloneContainer.classList.remove('active');
 
-
 const toggleIcons = document.querySelectorAll('.toggle-icon');
 const icons = document.querySelectorAll('.toggle-icon i');
-const darkcontainer = document.querySelector('#dark-container');
-
-const darkContainerImg = cloneContainer.querySelector('#dark-container .home-img img');
-
+const darkContainerImg = cloneContainer.querySelector('.home-img img');
 darkContainerImg.src = 'Manimain.PNG';
 
-toggleIcons.forEach(toggle =>{
-    toggle.addEventListener('click', () =>{
+toggleIcons.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.classList.add('disabled');
+    setTimeout(() => {
+      toggle.classList.remove('disabled');
+    }, 1500);
 
-        toggle.classList.add('disabled');
-        setTimeout(() => {
-            toggle.classList.remove('disabled');
-        }, 1500);
-
-        icons.forEach(icon => {
-            icon.classList.toggle('fa-sun');
-             icon.classList.toggle('fa-moon');
-           
-            
-        });
-
-        container.classList.toggle('active');
-        cloneContainer.classList.toggle('active');
-
+    icons.forEach(icon => {
+      icon.classList.toggle('fa-sun');
+      icon.classList.toggle('fa-moon');
     });
 
+    container.classList.toggle('active');
+    cloneContainer.classList.toggle('active');
+  });
+});
+
+const navLinks = document.querySelectorAll('.navbar a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    navLinks.forEach(nav => nav.classList.remove('active'));
+    this.classList.add('active');
+  });
 });
